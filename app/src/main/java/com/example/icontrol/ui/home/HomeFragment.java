a95ed294d5c6a1797dffb7322eb7ad03f3dec744
Dimethyl <mingchendai@outlook.com>
2024-12-11 19:44:25 +0800

    Update 2024.12.11

--- app/src/main/java/com/example/icontrol/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/icontrol/ui/home/HomeFragment.java
@@ -1,7 +1,10 @@
 package com.example.icontrol.ui.home;
 
+import static android.content.ContentValues.TAG;
+
 import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
 import android.content.Context;
 import android.content.SharedPreferences;
@@ -10,25 +13,29 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import com.example.icontrol.R;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.OutputStream;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.UUID;
 
 @RequiresApi(api = Build.VERSION_CODES.S)
@@ -36,7 +43,6 @@ public class HomeFragment extends Fragment {
 
     private ImageButton home_button_switch_1;
     private ImageButton home_button_switch_2;
-    private ImageButton home_button_refresh;
     private TextView home_textview_switch_status_1;
     private TextView home_textview_switch_status_2;
     private TextView home_textview_bluetooth_status;
@@ -53,17 +59,17 @@ public class HomeFragment extends Fragment {
             Manifest.permission.BLUETOOTH_CONNECT,
             Manifest.permission.BLUETOOTH_ADVERTISE
     };
+    private BluetoothSocket bluetoothSocket=null;
+    private final BluetoothDevice bluetoothDevice=bluetoothAdapter.getRemoteDevice("00:24:07:00:41:18");//00:24:07:00:41:18
     private int bluetoothAvailability=0;
     private boolean hasInitialized=false;
-
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View rootView=inflater.inflate(R.layout.fragment_home,container,false);
-
+        ImageButton home_button_refresh = rootView.findViewById(R.id.home_button_refresh);
         home_button_switch_1=rootView.findViewById(R.id.home_button_switch_1);
         home_button_switch_2=rootView.findViewById(R.id.home_button_switch_2);
-        home_button_refresh=rootView.findViewById(R.id.home_button_refresh);
         home_textview_switch_status_1=rootView.findViewById(R.id.home_textview_switch_status_1);
         home_textview_switch_status_2=rootView.findViewById(R.id.home_textview_switch_status_2);
         home_textview_bluetooth_status=rootView.findViewById(R.id.home_bluetooth_status);
@@ -76,12 +82,20 @@ public class HomeFragment extends Fragment {
     }
 
     private void init(boolean judge){
-        if(judge&&(bluetoothAvailability==2)){
+        if(judge&&(homeBluetoothConnected(bluetoothAdapter))){
             homeBluetoothRefreshUI(R.drawable.bluetooth_connected,R.string.home_bluetooth_status_ready);
         }
         else{
             homeBluetoothInit();
             if(bluetoothAvailability>0){
+                if(bluetoothSocket!=null){
+                    try{
+                        bluetoothSocket.close();
+                        Toast.makeText(requireContext(),"success",Toast.LENGTH_SHORT).show();
+                    } catch (IOException e) {
+                        Log.e(TAG,"INIT",e);
+                    }
+                }
                 homeBluetoothConnect();
             }
         }
@@ -137,36 +151,14 @@ public class HomeFragment extends Fragment {
     }
 
     private void homeBluetoothInit(){
-        home_textview_bluetooth_status.setText(R.string.home_bluetooth_status_initializing);
+        homeBluetoothRefreshUI(R.drawable.bluetooth_searching,R.string.home_bluetooth_status_initializing);
         if(bluetoothAdapter==null){
             bluetoothAvailability=-1;
             homeBluetoothRefreshUI(R.drawable.bluetooth_unavailable,R.string.home_bluetooth_status_init_fail_unsupported);
             return;
         }
-        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.S) {
-            boolean scanPermission = ContextCompat.checkSelfPermission(
-                    requireContext(),
-                    Manifest.permission.BLUETOOTH_SCAN
-            ) == PackageManager.PERMISSION_GRANTED;
-            boolean connectPermission = ContextCompat.checkSelfPermission(
-                    requireContext(),
-                    Manifest.permission.BLUETOOTH_CONNECT
-            ) == PackageManager.PERMISSION_GRANTED;
-            boolean advertisePermission = ContextCompat.checkSelfPermission(
-                    requireContext(),
-                    Manifest.permission.BLUETOOTH_ADVERTISE
-            ) == PackageManager.PERMISSION_GRANTED;
-            boolean adminPermission = ContextCompat.checkSelfPermission(
-                    requireContext(),
-                    Manifest.permission.BLUETOOTH_ADMIN
-            ) == PackageManager.PERMISSION_GRANTED;
-            boolean Permission = ContextCompat.checkSelfPermission(
-                    requireContext(),
-                    Manifest.permission.BLUETOOTH
-            ) == PackageManager.PERMISSION_GRANTED;
-            if((!scanPermission)||(!advertisePermission)||(!connectPermission)||(!adminPermission)||(!Permission)){
-                requestMultiplePermissionsLauncher.launch(PERMISSIONS);
-            }
+        if((Build.VERSION.SDK_INT>=Build.VERSION_CODES.S)&&(!homeBluetoothAuthorized())) {
+            requestMultiplePermissionsLauncher.launch(PERMISSIONS);
         }
         homeBluetoothEnable();
     }
@@ -202,15 +194,60 @@ public class HomeFragment extends Fragment {
 
     private void homeBluetoothConnect(){
         homeBluetoothRefreshUI(R.drawable.bluetooth_searching,R.string.home_bluetooth_status_connecting);
-        bluetoothAdapter.startDiscovery();
-        try{
-            BluetoothSocket bluetoothSocket=bluetoothAdapter.getRemoteDevice("00:24:07:00:41:18").createRfcommSocketToServiceRecord(UUID.fromString("00001101-0000-1000-8000-00805f9b34fb"));
-            bluetoothSocket.connect();
-            bluetoothAvailability=2;
-            homeBluetoothRefreshUI(R.drawable.bluetooth_connected,R.string.home_bluetooth_status_ready);
+        if(ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.BLUETOOTH_SCAN)!= PackageManager.PERMISSION_GRANTED) {
+            homeBluetoothRefreshUI(R.drawable.bluetooth_unavailable,R.string.home_bluetooth_status_init_fail_unauthorized);
         }
-        catch(IOException e){
-            homeBluetoothRefreshUI(R.drawable.bluetooth_ready,R.string.home_bluetooth_connect_fail);
+        else{
+            //bluetoothAdapter.startDiscovery();
+            try{
+                bluetoothSocket=bluetoothDevice.createInsecureRfcommSocketToServiceRecord(UUID.fromString("00001101-0000-1000-8000-00805f9b34fb"));
+                ConnectThread connectThread = new ConnectThread(bluetoothSocket, true);
+                connectThread.start();
+            }
+            catch(IOException e){
+                homeBluetoothRefreshUI(R.drawable.bluetooth_unconnected,R.string.home_bluetooth_connect_fail);
+                Log.e(TAG,"homeBluetoothConnect:",e);
+            }
+        }
+    }
+    
+    private class ConnectThread extends Thread{
+        private BluetoothSocket bluetoothSocket;
+        private boolean activeConnect;
+        private ConnectThread(BluetoothSocket bluetoothSocket,boolean connect){
+            this.bluetoothSocket=bluetoothSocket;
+            this.activeConnect=connect;
+        }
+
+        @Override
+        public void run(){
+            if(activeConnect){
+                try{
+                    bluetoothSocket=bluetoothDevice.createRfcommSocketToServiceRecord(UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"));
+                    //bluetoothSocket=(BluetoothSocket)bluetoothDevice.getClass().getMethod("createRfcommSocket", new Class[] {int.class}).invoke(bluetoothDevice,1);
+                } catch (Exception e) {
+                    Log.e(TAG,"ConnectThread",e);
+                }
+                try{
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    Log.e(TAG,"ConnectThread",e);
+                }
+                new Thread(){
+                    @Override
+                    public void run(){
+                        try{
+                            bluetoothSocket.connect();
+                            homeBluetoothRefreshUI(R.drawable.bluetooth_connected,R.string.home_bluetooth_status_connected);
+                            bluetoothAvailability=2;
+                        }catch (IOException e) {
+                            homeBluetoothRefreshUI(R.drawable.bluetooth_unconnected,R.string.home_bluetooth_connect_fail);
+                            bluetoothAvailability=1;
+                            Log.e(TAG,"ConnectThread",e);
+                        }
+                    }
+                }.start();
+            }
         }
     }
 
@@ -223,14 +260,61 @@ public class HomeFragment extends Fragment {
     }
 
     public boolean homeBluetoothSend(String signal){
-        try{
-            BluetoothSocket bluetoothSocket=bluetoothAdapter.getRemoteDevice("00:24:07:00:41:18").createRfcommSocketToServiceRecord(UUID.fromString("00001101-0000-1000-8000-00805f9b34fb"));
-            OutputStream outputStream=bluetoothSocket.getOutputStream();
-            outputStream.write(signal.getBytes());
-        } catch (IOException e) {
-            return false;
+        if(ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED){
+            homeBluetoothRefreshUI(R.drawable.bluetooth_unavailable,R.string.home_bluetooth_status_init_fail_unauthorized);
+        }
+        else{
+            try{
+
+                bluetoothSocket=bluetoothDevice.createRfcommSocketToServiceRecord(UUID.fromString("00001101-0000-1000-8000-00805f9b34fb"));
+                OutputStream outputStream=bluetoothSocket.getOutputStream();
+                bluetoothSocket.getOutputStream().write(signal.getBytes(),0,1);
+            }
+            catch(IOException e){
+                Log.e(TAG,"homeBluetoothSend:",e);
+                return false;
+            }
         }
         return true;
+    }
+
+    boolean homeBluetoothAuthorized(){
+        boolean scanPermission = ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.BLUETOOTH_SCAN
+        ) == PackageManager.PERMISSION_GRANTED;
+        boolean connectPermission = ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.BLUETOOTH_CONNECT
+        ) == PackageManager.PERMISSION_GRANTED;
+        boolean advertisePermission = ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.BLUETOOTH_ADVERTISE
+        ) == PackageManager.PERMISSION_GRANTED;
+        boolean adminPermission = ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.BLUETOOTH_ADMIN
+        ) == PackageManager.PERMISSION_GRANTED;
+        boolean Permission = ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.BLUETOOTH
+        ) == PackageManager.PERMISSION_GRANTED;
+        return (scanPermission)&&(connectPermission)&&(advertisePermission)&&(adminPermission)&&(Permission);
+    }
+
+    boolean homeBluetoothConnected(BluetoothAdapter bluetoothAdapter){
+        try{
+            Method method=bluetoothAdapter.getClass().getDeclaredMethod("getConnectionState", (Class[]) null);
+            method.setAccessible(true);
+            int state=(int)method.invoke(bluetoothAdapter,(Object[]) null);
+            if(state==BluetoothAdapter.STATE_CONNECTED){
+                return true;
+            }
+        }
+        catch(NoSuchMethodException | IllegalAccessException | InvocationTargetException e){
+            Log.e(TAG,"NoSuchMethodException",e);
+        }
+        return false;
     }
 
     @Override
--- app/src/main/java/com/example/icontrol/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/icontrol/ui/notifications/NotificationsFragment.java
@@ -7,29 +7,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-//import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-//import androidx.lifecycle.ViewModelProvider;
 
 import com.example.icontrol.R;
-//import com.example.icontrol.databinding.FragmentNotificationsBinding;
 public class NotificationsFragment extends Fragment{
-
-    //public FragmentNotificationsBinding binding;
     public Button notifications_button_github;
     public Button notifications_button_license;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        /*
-                NotificationsViewModel notificationsViewModel =
-                new ViewModelProvider(this).get(NotificationsViewModel.class);
-                binding = FragmentNotificationsBinding.inflate(inflater, container, false);
-                */
-
         View rootView=inflater.inflate(R.layout.fragment_notifications,container,false );
         notifications_button_github = rootView.findViewById(R.id.notifications_button_github);
         notifications_button_license = rootView.findViewById(R.id.notifications_button_license);
@@ -43,15 +32,11 @@ public class NotificationsFragment extends Fragment{
             intent.setData(Uri.parse("https://www.gnu.org/licenses/gpl-3.0.en.html#license-text"));
             startActivity(intent);
         });
-
-        //final TextView textView = binding.textNotifications;
-        //notificationsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return rootView;
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
-        //binding = null;
     }
 }
